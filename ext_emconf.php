4213b5a51bb6540854b80c5046bf189dfff0b08f
jonathanheilmann <mail@jonathan-heilmann.de>
2015-06-05 20:25:17 +0200

    [TASK] Set version to 0.1.0-dev and removed support for TYPO3 CMS 6.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,39 +1,39 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "jh_ter_announcer".
-#
-# Auto generated 03-05-2015 17:40
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "jh_ter_announcer".
+ *
+ * Auto generated 12-08-2014 20:15
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'TER Announcer',
 	'description' => 'Provides a task to send you a mail if an extension-update is available.',
 	'category' => 'services',
-	'shy' => 1,
-	'version' => '0.0.4',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'TYPO3_version' => '6.1.0 - 6.2.99',
-	'PHP_version' => '',
-	'module' => '',
+	'version' => '0.1.0-dev',
 	'state' => 'beta',
-	'uploadfolder' => '',
+	'uploadfolder' => true,
 	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 1,
-	'lockType' => '',
+	'clearcacheonload' => true,
 	'author' => 'Jonathan Heilmann',
 	'author_email' => 'mail@jonathan-heilmann.de',
 	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
+	'constraints' =>
+	array (
+		'depends' =>
+		array (
+			'typo3' => '6.2.0 - 6.2.99',
+		),
+		'conflicts' =>
+		array (
+		),
+		'suggests' =>
+		array (
+		),
+	),
 );
 
-?>
