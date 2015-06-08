abe83826a9abe4602b3e667bc28841f4017c3524
jonathanheilmann <mail@jonathan-heilmann.de>
2015-06-08 14:02:30 +0200

    [ENHANCEMENT] Add rss feed (Related to #3)
    Re-add tasks

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,12 +8,27 @@ if (!defined('TYPO3_MODE')) {
 	'Extension',
 	array(
 		'Extension' => 'list, show',
-		
+
 	),
 	// non-cacheable actions
 	array(
 		'Extension' => '',
-		
+
 	)
 );
 ## EXTENSION BUILDER DEFAULTS END TOKEN - Everything BEFORE this line is overwritten with the defaults of the extension builder
+
+// Add TER Announcer task
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['Heilmann\\JhTerAnnouncer\\Task\\TerAnnouncerTask'] = array(
+	'extension' => $_EXTKEY,
+	'title' => 'TER Announcer',
+	'description' => 'Announces new ext-versions',
+	'additionalFields' => '\\Heilmann\\JhTerAnnouncer\\Task\\TerAnnouncerAdditionalFieldProvider'
+);
+
+// Add task to update from TER
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['Heilmann\\JhTerAnnouncer\\Task\\UpdateFromTerTask'] = array(
+	'extension' => $_EXTKEY,
+	'title' => 'Update from TER',
+	'description' => 'Update extension list from TER'
+);
